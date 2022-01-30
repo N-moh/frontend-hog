855876f1c9dd73490bd4d2cc6f79c624dcc29fe2
senorsyzygy <45519112+senorsyzygy@users.noreply.github.com>
2022-01-30 15:17:53 +0000

    Images go to AWS

--- src/components/userslogin/EmpProfilecard.jsx
+++ src/components/userslogin/EmpProfilecard.jsx
@@ -23,8 +23,8 @@ function SProfilecard(props){
             <Card id="main"className="Card "  style={{ width: '14rem' }} >
                 <Card.Body >
                 <Card.Img  className="mh-10 mw-10 hover-shadow cardImg"  src={props.picture} alt="" />
-                <Card.Text><h3> {props.firstname}{props.lastname}</h3></Card.Text>
-                <Card.Text>{props.bio} </Card.Text>
+                <Card.Text><h3> {props.firstname} {props.lastname}</h3></Card.Text>
+                <Card.Text>{props.bio}</Card.Text>
                 <Card.Link href={"mailto:"+props.email}> <FontAwesomeIcon icon = {faEnvelope}></FontAwesomeIcon></Card.Link>
                 <Card.Link target="_blank" href={props.github}><FontAwesomeIcon icon = {faGithub}></FontAwesomeIcon></Card.Link>
                 <Card.Link target="_blank" href={props.linkedin}><FontAwesomeIcon icon = {faLinkedin}></FontAwesomeIcon></Card.Link>
@@ -35,7 +35,7 @@ function SProfilecard(props){
                     <Card.Text>{props.cv}</Card.Text>
                     <Card.Text>Hired: {props.hired}</Card.Text>
                     <Card.Text>Course: {props.course}</Card.Text>
-                    <Card.Text>Start date:<Moment format="DD MMM yyyy" >{props.date}</Moment></Card.Text>
+                    <Card.Text>Start date:<Moment format="DD MMM yyyy" > {props.date}</Moment></Card.Text>
                     <Button class="see-less-btn" onClick={() => setShow(!show)}>See less</Button>
                     <br />
                     <br />
