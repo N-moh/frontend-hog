b45e1b2cf45111a04302b9159e2e4d301dc5f947
N-moh <93383553+N-moh@users.noreply.github.com>
2022-02-02 20:03:02 +0000

    deleted unnecessary dependencies

--- src/components/userslogin/Profcard.jsx
+++ src/components/userslogin/Profcard.jsx
@@ -6,7 +6,7 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
+//import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
