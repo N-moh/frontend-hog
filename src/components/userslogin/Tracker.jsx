1fd32dc8da7b3436635e44614c746923c225951b
N-moh <93383553+N-moh@users.noreply.github.com>
2022-02-02 19:53:41 +0000

    created Progress tracker component

---
+++ src/components/userslogin/Tracker.jsx
@@ -0,0 +1,23 @@
+//import Frame from 'react-frame-component';
+import React from 'react';
+
+function Tracker() {
+
+  return (
+    <div >
+        <h2>HOG Program success tracker</h2>
+    
+        <iframe  width="640" height="480" src="https://charts.mongodb.com/charts-best-hog-group-qcbap/embed/charts?id=3e0f61ad-8e39-45b2-bda5-1db52d1572e3&maxDataAge=3600&theme=dark&autoRefresh=true"></iframe>
+        
+    </div>
+  );
+}
+
+export default Tracker;
+
+
+
+
+
+
+
